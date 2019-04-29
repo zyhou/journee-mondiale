6ddb855397df8e4d7cf80ba320263457ec9a5474
zyhou <richard.maxime9@gmail.com>
2019-04-29 13:04:26 +0200

    bootstrap

---
+++ README.md
@@ -0,0 +1,1 @@
+## Journée mondiale
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import format from 'date-fns/format';
+import startOfToday from 'date-fns/start_of_today';
+import locale from 'date-fns/locale/fr';
+import { render, Box, Color } from 'ink';
+import days from './days.js';
+
+const today = startOfToday();
+const longDay = format(today, 'dddd Do MMMM YYYY', { locale });
+const shortDay = format(today, 'MM-DD');
+
+const day = days[shortDay];
+
+const MondialDays = () => {
+    return (
+        <React.Fragment>
+            <Box marginTop={1}>
+                <Color cyan>Nous sommes le {longDay}</Color>
+            </Box>
+            {day && day.map(day => <Box key={day.text}>{day.text}</Box>)}
+            {!day && 'Pas de jour'}
+        </React.Fragment>
+    );
+};
+
+render(<MondialDays />);
